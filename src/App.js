8b494f3c8f71c9ff19cb4cff28e999ac258ffc4a
David Ihl <daih1985@gmail.com>
2023-05-12 00:09:48 +0200

    Add form fields

--- src/App.js
+++ src/App.js
@@ -1,3 +1,55 @@
-export default function App() {
-  return <div>Hallo</div>;
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Icon,
+  Paper,
+  TextField,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
+
+function App() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Icon>
+            <PeopleAltIcon sx={{ paddingRight: '8px' }} />
+          </Icon>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Guest list
+          </Typography>
+          <Button color="inherit">Clear all</Button>
+        </Toolbar>
+      </AppBar>
+      <LinearProgress color="secondary" />
+      <Container maxWidth="md">
+        <Paper position="static" sx={{ marginTop: '24px', padding: '24px' }}>
+          <TextField
+            sx={{ flexGrow: 1 }}
+            id="filled-basic"
+            label="Filled"
+            variant="filled"
+            fullWidth
+          />
+          <TextField
+            sx={{ flexGrow: 1 }}
+            id="filled-basic"
+            label="Filled"
+            variant="filled"
+            fullWidth
+          />
+        </Paper>
+        <Paper position="static" sx={{ marginTop: '16px' }}>
+          Test
+        </Paper>
+      </Container>
+    </Box>
+  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2b2b2b',
+    },
+    secondary: {
+      main: '#00bcd4',
+    },
+    background: {
+      default: '#f5f5f5',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
